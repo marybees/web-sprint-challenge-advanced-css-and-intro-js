e01470cf7c44781aa2004f431e00c91d4156f780
Mary Baker <meb601@gmail.com>
2020-07-17 12:26:23 -0400

    Complete JavaScript Task 3

--- index.js
+++ index.js
@@ -225,9 +225,11 @@ console.log(artists[8].name);
  * For example, if getArtistByIndex is invoked with the artists dataset and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
-function getArtistByIndex(array, index) {
-    /* code here */
+function getArtistByIndex(artistArray, artistArrayIndex) {
+   return `The artist at index ${artistArrayIndex} is ${artistArray[artistArrayIndex].name}.`
   }
+
+  console.log(getArtistByIndex(artists, 0));
   
   /**
 
